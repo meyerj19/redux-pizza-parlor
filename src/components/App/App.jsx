055c609bbb79003981e9e708c0be2f7fd0ed176d
Travis Huss <travisjhuss@gmail.com>
2021-01-21 13:40:19 -0600

    added component files and folders

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Prime Pizza</h1>
+        </header>
+
+        <img src='images/pizza_photo.png' />
+        <p>Pizza is great.</p>
+
+      </div>
+    </Router>
   );
 }
 
